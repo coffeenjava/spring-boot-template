a377716c315b8eca06243b44edc6505ecf7b45f6
coffeenjava <coffeenjs@gmail.com>
2023-09-14 15:58:35 +0900

    step-1

--- my-api/src/main/java/com/template/myapi/service/UserService.java
+++ my-api/src/main/java/com/template/myapi/service/UserService.java
@@ -2,9 +2,14 @@ package com.template.myapi.service;
 
 import com.template.myapi.controller.req.UserCreateReq;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import java.util.regex.Pattern;
 
 @Service
 public class UserService {
+
+    private static Pattern pattern = Pattern.compile("^010-\\d{3,4}-\\d{4}$");
 
     public int create(UserCreateReq req) {
         if (req.getAge() <= 10) {
@@ -13,6 +18,10 @@ public class UserService {
 
         if (req.getAge() >= 18 && req.getNationalId() == null) {
             throw new RuntimeException("18세 이상은 주민등록번호가 필요합니다.");
+        }
+
+        if (StringUtils.hasText(req.getPhone()) && pattern.matcher(req.getPhone()).matches()) {
+            throw new RuntimeException("휴대폰 번호 형식이 올바르지 않습니다.");
         }
 
         // user 생성 관련 비즈니스 로직
