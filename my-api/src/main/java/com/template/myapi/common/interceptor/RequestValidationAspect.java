a8ae24f36187acec704db725a6cc9dbb4d169c8b
coffeenjava <coffeenjs@gmail.com>
2023-09-14 18:12:34 +0900

    step-4-1

--- my-api/src/main/java/com/template/myapi/common/interceptor/RequestValidationAspect.java
+++ my-api/src/main/java/com/template/myapi/common/interceptor/RequestValidationAspect.java
@@ -6,16 +6,26 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
+import java.util.Collection;
+
 @Aspect
 @Component
 public class RequestValidationAspect {
 
     @Before("bean(*Controller) && " +
-            "(execution(* *(.., com.template.myapi.controller.req.BaseRequest+, ..)))")
+            "(execution(* *(.., com.template.myapi.controller.req.BaseRequest+, ..)) || " +
+            "execution(* *(.., java.util.Collection<com.template.myapi.controller.req.BaseRequest+>+, ..)))")
     public void before(final JoinPoint joinPoint) {
         for (final Object arg : joinPoint.getArgs()) {
             if (arg instanceof BaseRequest) {
                 ((BaseRequest) arg).validate();
+            } else if (arg instanceof Collection) {
+                Collection dtos = (Collection) arg;
+                dtos.forEach(dto -> {
+                    if (dto instanceof BaseRequest) {
+                        ((BaseRequest) dto).validate();
+                    }
+                });
             }
         }
     }
--- my-api/src/main/java/com/template/myapi/controller/UserController.java
+++ my-api/src/main/java/com/template/myapi/controller/UserController.java
@@ -3,13 +3,16 @@ package com.template.myapi.controller;
 import com.template.myapi.controller.req.UserCreateReq;
 import com.template.myapi.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.List;
 
+@Validated
 @RequestMapping("/users")
 @RestController
 public class UserController {
@@ -23,5 +26,11 @@ public class UserController {
     @PostMapping
     public int create(@Valid @RequestBody UserCreateReq req) {
         return userService.create(req);
+    }
+
+    @PostMapping("/list")
+    public String create(@Valid @RequestBody List<UserCreateReq> reqList) {
+        System.out.println(reqList);
+        return "ok";
     }
 }
